50ecd1af997adad625da7368b061a5647479d594
Allan Santana <santana.rod15@gmail.com>
2022-02-17 18:46:29 -0300

    new ConstantsTemplate

---
+++ youtubeAppCC/ConstantsTemplate.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsTemplate.swift
+//  youtubeAppCC
+//
+//  Created by Allan Santana on 17/02/22.
+//
+
+
+import Foundation
+
+
+struct ConstantsTemplate {
+    
+    static var API_KEY = "XXXXXXXXXXXXX"
+    static var PLAYLIST_ID = "PLMRqhzcHGw1aMOUiH5tq-rlyvpYHwGluV"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(PLAYLIST_ID)&key=\(API_KEY)"
+    
+    
+    
+}
